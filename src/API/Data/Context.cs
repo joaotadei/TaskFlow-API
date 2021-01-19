47287e62a809e8707e5d0bc2f1b9193327653b3c
joao.vitortadei <joao.vitortadei@gmail.com>
2021-01-19 20:31:07 -0300

    Configuração banco in memory

---
+++ src/API/Data/Context.cs
@@ -0,0 +1,14 @@
+﻿using API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options) { }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<ToDoItem> ToDoItems { get; set; }
+
+    }
+}
