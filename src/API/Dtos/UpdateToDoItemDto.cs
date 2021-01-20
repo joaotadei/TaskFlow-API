dbb8f2423c80a4ecce73a079fdaec2638938e656
joao.vitortadei <joao.vitortadei@gmail.com>
2021-01-20 09:25:55 -0300

    dto de update item

---
+++ src/API/Dtos/UpdateToDoItemDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace API.Dtos
+{
+    public class UpdateToDoItemDto
+    {
+        public Guid Id { get; set; }
+        public string Description { get; set; }
+        public DateTime Expiration { get; set; }
+    }
+}
