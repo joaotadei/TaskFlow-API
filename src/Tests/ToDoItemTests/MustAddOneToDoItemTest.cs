5be494f63b0bbd82e3927a9629d5d9e48fc73013
joao.vitortadei <joao.vitortadei@gmail.com>
2021-01-21 21:08:05 -0300

    [WIP] testes

---
+++ src/Tests/ToDoItemTests/MustAddOneToDoItemTest.cs
@@ -0,0 +1,28 @@
+﻿using API.Helpers;
+using API.Models;
+using System;
+using Xunit;
+
+namespace Tests.ToDoItemTests
+{
+    public class MustAddOneToDoItemTest
+    {
+        [Fact]
+        public void MustAddOneToDoItem()
+        {
+            //arrange
+            var description = "Concluir api amanhã";
+            var expiration = DateTime.Now.AddDays(1);
+            var user = new User("joao@joao.com", "password", AccountHelper.DefaultUserRole);
+            var newItem = new ToDoItem(description, expiration);
+
+            //action
+            user.AddToDoItem(newItem);
+
+            //assert
+            Assert.Single(user.ToDoItems);
+            Assert.Equal(description, user.ToDoItems[0].Description);
+            Assert.Equal(expiration, user.ToDoItems[0].Expiration);
+        }
+    }
+}
